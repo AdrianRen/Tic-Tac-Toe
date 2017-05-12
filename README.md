46b17f362ae49f0a9782030abad95aa779c73059
Adrian Ren <adrian.ren@outlook.com>
2017-05-12 22:18:22 +1200

    updates

--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 ## Project Requirements:
 
 -   [x] Use the supplied mockup files and HTML snippets to guide you in building a Tic Tac Toe game. You can use jQuery or plain JavaScript to complete this project. Don't use an already programmed Tic Tac Toe plugin or library.
--   [ ] When the page loads, the startup screen should appear.
--   [ ] Add programming, so that when the player clicks the start button the start screen disappears, the board appears, and the game begins.
--   [ ] Add the game play following these rules::
+-   [x] When the page loads, the startup screen should appear.
+-   [x] Add programming, so that when the player clicks the start button the start screen disappears, the board appears, and the game begins.
+-   [x] Add the game play following these rules::
     -   Play alternates between X and O.
     -   The current player is indicated at the top of the page -- the box with the symbol O or X is highlighted for the current player.
     -   When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square.
     -   Players can only click on empty squares. When the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square.
     -   The game ends when one player has three of their symbols in a row either horizontally, vertically or diagonally. If all of the squares are filled and no players have three in a row, the game is a tie.
--   [ ] Add programming so that when the game ends, the board disappears and the game end screen appears:
+-   [x] Add programming so that when the game ends, the board disappears and the game end screen appears:
     -   Show the word "Winner" or the phrase "It's a Tie!"
     -   Add the appropriate class to the ```<div> for the winning screen: <div class="screen screen-win" id="finish">``` screen-win-one for player 1, screen-win-two for player two, or screen-win-tie if the game ends with no winner. For example, if player 1 wins, the HTML should look like this: ```<div class="screen screen-win screen-win-one" id="finish">```
--   [ ] Add programming so that when a player pushes the "New Game" button, the board appears again, empty, and a new game begins.
--   [ ] Use the module pattern to wrap all of your JavaScript code into a single global variable or an immediately invoked function.
+-   [x] Add programming so that when a player pushes the "New Game" button, the board appears again, empty, and a new game begins.
+-   [x] Use the module pattern to wrap all of your JavaScript code into a single global variable or an immediately invoked function.
 
 ## Extra Credit:
 
--- js/app.js
+++ js/app.js
@@ -1,85 +1,159 @@
-(function(){
+(function() {
   let playerOne = '#player1';
   let playerTwo = '#player2';
-  // set startPage with content of start.txt
-  let startPage =
-  `<div class="screen screen-start" id="start">
-    <header>
-      <h1>Tic Tac Toe</h1>
-      <a href="#" class="button">Start game</a>
-    </header>
-  </div>`;
-  // set resultPage with content of win.txt
-  let resultPage =
-  `<div class="screen screen-win" id="finish">
-    <header>
-      <h1>Tic Tac Toe</h1>
-      <p class="message"></p>
-      <a href="#" class="button">New game</a>
-    </header>
-  </div>`;
-  //append them to to the body
-  $('body').append(startPage)
-           .append(resultPage);
-  // hide them for future use
+  let winner = "";
+  // Set letiables for start and end screens
+  let startEnd = {
+    start: "<div class='screen screen-start' id='start'>" +
+      "<header><h1>Tic Tac Toe</h1>" +
+      "<a href='#' class='button'>Play Game</a><br>" +
+      "</header></div>",
+    end: "<div class='screen screen-win' id='finish'>" +
+      "<header><h1>Tic Tac Toe</h1>" +
+      "<p class='message'></p>" +
+      "<a href='#' class='button'>New game</a>" +
+      "</header></div>"
+  }; // Append the start & finish screens to the body, then hide them
+  $('body').append(startEnd.start)
+           .append(startEnd.end);
   $('#start, #finish').hide();
 
-  /**
-   *  Step 1: The startup screen should appear when the page *          loads
-  **/
-  (function () {
-      $('#start').show();
-      $('.button').on('click',function() {
-        $('#start, #finish').hide();
-        $('#board').show();
-        $('.box').each(function() {
-          this.style.backgroundImage = 'none';
-          $(this).removeClass('box-filled-1')
-                 .removeClass('box-filled-2');
-        });
-        // reset all the previous actived class
-        $('li.players').removeClass('active');
-        playGame();
+  // Load the starting screen on page load
+  (function() {
+    $('#start').show();
+    $('.button').on('click', function() {
+      $('#start, #finish').hide();
+      $('#board').show();
+      $(".box").each(function() {
+        this.style.backgroundImage = "none";
+        $(this).removeClass("box-filled-1");
+        $(this).removeClass("box-filled-2");
       });
+      //Remove any previously assigned active classes
+      $('li.players').removeClass('active');
+      //Randomly assign starting user to keep game fair
+      let li = $("ul li");
+      let rand = Math.floor(Math.random() * 2);
+      li.eq(rand % li.length).addClass("active");
+      playGame();
+    });
   }());
 
-  /**
-   * Step 2
-  **/
-  let playGame = function () {
+  // Start the gameplay
+  let playGame = function() {
     $('.box').each(function() {
-      $(this).mouseenter(function() {
-        /* Act on the event */
-        if ($(playerOne).hasClass('active')) {
-          this.style.backgroundImage = 'url("img/o.svg")';
+      $(this).mouseenter(function() { // Add the background image on hover
+        if ($(playerOne).hasClass("active")) {
+          this.style.backgroundImage = "url('img/o.svg')";
         } else {
-          this.style.backgroundImage = 'url("img/x.svg")'
+          this.style.backgroundImage = "url('img/x.svg')";
         }
       });
-      $(this).mouseleave(function() {
-        /* Act on the event */
-        this.style.backgroundImage = 'none';
+      $(this).mouseleave(function() { // On mouseleave, remove the background image
+        this.style.backgroundImage = "none";
       });
     });
-    $('.box').click(function() {
-      /* Act on the event */
-      if($(playerOne).hasClass('active')){
-        if ($(this).hasClass('box-filled-1') === false && $(this).hasClass('box-filled-2') === false) {
+    $('.box').click(function() { // On click, check to see if user already placed piece down. If not, then add class box-filled, add background image, unbind the mouseleave function, fire nextTurn function to switch active class to next player. Check for win condition.
+      if ($(playerOne).hasClass("active")) {
+        if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
           $(this).addClass('box-filled-1');
-          this.style.backgroundImage = 'url("img/o.svg")';
+          this.style.backgroundImage = "url('img/o.svg')";
           $(this).unbind('mouseenter mouseleave');
-          // checkWin();
-          // nextTurn();
+          checkWin();
+          nextTurn();
         }
-      } else if($(playerTwo).hasClass("active")){
-        if ($(this).hasClass('box-filled-1') === false && $(this).hasClass('box-filled-2') === false) {
+      } else if ($('#player2').hasClass("active")) {
+        if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
           $(this).addClass('box-filled-2');
-          this.style.backgroundImage = 'url("img/x.svg")';
+          this.style.backgroundImage = "url('img/x.svg')";
           $(this).unbind('mouseenter mouseleave');
-          // checkWin();
-          // nextTurn();
-        } 
+          checkWin();
+          nextTurn();
+        }
+      } else {
+        console.log("No more turns!");
+      }
+
+    });
+  };
+
+  let nextTurn = function() {
+    if ($(playerOne).hasClass('active')) {
+      $(playerOne).removeClass("active");
+      $(playerTwo).addClass("active");
+    } else {
+      $(playerTwo).removeClass("active");
+      $(playerOne).addClass("active");
+    }
+  };
+
+  let checkWin = function() {
+    // Create empty arrary of moves
+    let winning = [];
+    //Loop over boxes and add currently placed piece
+    $('.box').each(function() {
+      if ($(this).hasClass('box-filled-1')) {
+        winning.push("player1");
+      } else if ($(this).hasClass('box-filled-2')) {
+        winning.push("player2");
+      } else {
+        winning.push("empty");
       }
     });
-  }
+    // Check the array to find winning combinations. If pieces match one in 8 possible combinations, set the winner to matching name
+    if (winning[0] !== "empty" && winning[0] === winning[1] && winning[1] === winning[2]) {
+      winner = winning[0];
+      showWinner();
+    } else if (winning[3] !== "empty" && winning[3] === winning[4] && winning[4] === winning[5]) {
+      winner = winning[3];
+      showWinner();
+    } else if (winning[6] !== "empty" && winning[6] === winning[7] && winning[7] === winning[8]) {
+      winner = winning[6];
+      showWinner();
+    } else if (winning[0] !== "empty" && winning[0] === winning[3] && winning[3] === winning[6]) {
+      winner = winning[0];
+      showWinner();
+    } else if (winning[1] !== "empty" && winning[1] === winning[4] && winning[4] === winning[7]) {
+      winner = winning[1];
+      showWinner();
+    } else if (winning[2] !== "empty" && winning[2] === winning[5] && winning[5] === winning[8]) {
+      winner = winning[2];
+      showWinner();
+    } else if (winning[0] !== "empty" && winning[0] === winning[4] && winning[4] === winning[8]) {
+      winner = winning[0];
+      showWinner();
+    } else if (winning[2] !== "empty" && winning[2] === winning[4] && winning[4] === winning[6]) {
+      winner = winning[2];
+      showWinner();
+    } else if (winning.includes("empty") === false) {
+      winner = "Tie Game";
+      showWinner();
+    }
+    console.log(winning);
+  };
+  // Display appropriate winning screen based on who won, or if it was a tie.
+  let showWinner = function() {
+    if (winner === "player1") {
+      $("#finish").removeClass("screen-win-two");
+      $("#finish").removeClass("screen-win-tie");
+      $(".message").html("Winner");
+      $("#finish").addClass("screen-win-one");
+      $("#finish").show();
+      $("#board").hide();
+    } else if (winner === "player2") {
+      $("#finish").removeClass("screen-win-one");
+      $("#finish").removeClass("screen-win-tie");
+      $(".message").html("Winner");
+      $("#finish").addClass("screen-win-two");
+      $("#finish").show();
+      $("#board").hide();
+    } else if (winner === "Tie Game") {
+      $("#finish").removeClass("screen-win-one");
+      $("#finish").removeClass("screen-win-two");
+      $(".message").html("It's a Tie!");
+      $("#finish").addClass("screen-win-tie");
+      $("#finish").show();
+      $("#board").hide();
+    }
+  };
 }());
